d6c5093dce4bf9c30e30027df3218283f081b3a0
Malik <malik@hey.com>
2024-03-18 15:38:39 +0100

    Added 60 exercises to definitions chapter.

---
+++ app/logic/informal/definitions/[...exercise]/page.tsx
@@ -0,0 +1,31 @@
+'use client';
+import Exercise from '@/components/exercise';
+import ExerciseSidebar from '@/components/exerciseSidebar';
+import { usePathname } from 'next/navigation';
+
+export default function ExercisePage({
+  params,
+}: {
+  params: { exercise: number };
+}) {
+  const pathname = usePathname();
+  return (
+    <>
+      <div className='flex w-full h-screen overflow-scroll'>
+        <ExerciseSidebar
+          chapter={3}
+          path='/logic/informal/definitions/'
+          isQuestionActive={(index) => {
+            return pathname === `/logic/informal/definitions/${index + 1}`;
+          }}
+          initialQuestionIdx={0}
+        />
+        <div className='p-4 w-full'>
+          <h1 className='mb-6 text-3xl font-bold text-stone-900'>Quiz Q</h1>
+          {/* We're subtracting 1 from the parameters because the index of the exercises starts at 0 */}
+          <Exercise chapter={3} initialQuestionIdx={params.exercise - 1} />
+        </div>
+      </div>
+    </>
+  );
+}
