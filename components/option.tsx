9ed41d9d626bffe02a2d51cde61478dfae8515b7
Malik <malik@hey.com>
2024-03-25 00:41:00 +0100

    Constraining keyboard navigation so users don't select options that don't exist. Making selected options focusable with refs.

--- components/option.tsx
+++ components/option.tsx
@@ -1,35 +1,41 @@
 import classNames from 'classnames';
+import React from 'react';
 
 export interface OptionProps {
+  ref?: React.Ref<HTMLButtonElement>;
   label: string;
   isActive: boolean;
   isCorrect: boolean;
   showSolution: boolean;
 
   onClick: () => void;
-  onKeyDown?: (event: any) => void;
+  onKeyDown?: (event: React.KeyboardEvent<HTMLButtonElement>) => void;
 }
-export default function Option({
-  label,
-  isActive,
-  isCorrect,
-  showSolution,
 
-  onClick,
-  onKeyDown,
-}: OptionProps) {
-  const className = classNames(
-    'w-full ps-4 text-primary flex items-center border rounded-lg',
-    !isActive && 'border-stone-200',
-    showSolution && (isCorrect ? 'bg-green-100' : 'border-rose-200'),
-    !showSolution && isActive && 'border-primary border-4'
-  );
+const Option = React.forwardRef<HTMLButtonElement, OptionProps>(
+  ({ label, isActive, isCorrect, showSolution, onClick, onKeyDown }, ref) => {
+    const className = classNames(
+      'w-full ps-4 text-primary flex items-center border rounded-lg',
+      !isActive && 'border-stone-200',
+      showSolution && (isCorrect ? 'bg-green-100' : 'border-rose-200'),
+      !showSolution && isActive && 'border-primary border-4'
+    );
 
-  return (
-    <button onClick={onClick} className={className} onKeyDown={onKeyDown}>
-      <div className='flex'>
-        <div className='py-4 ms-2 font-medium text-stone-900'>{label}</div>
-      </div>
-    </button>
-  );
-}
+    return (
+      <button
+        ref={ref}
+        onClick={onClick}
+        className={className}
+        onKeyDown={onKeyDown}
+      >
+        <div className='flex'>
+          <div className='py-4 ms-2 font-medium text-stone-900'>{label}</div>
+        </div>
+      </button>
+    );
+  }
+);
+
+Option.displayName = 'Option'; // Add display name here
+
+export default Option;
--- components/quiz.tsx
+++ components/quiz.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Option from './option';
 import { chapters } from '@/content';
 import Prompt from './prompt';
@@ -46,6 +46,14 @@ const Quiz: React.FC<QuizProps> = ({
     }
   };
 
+  // Creating refs to make each option focusable with the keyboard shorcuts.
+  const activeOptionRef = useRef<HTMLButtonElement>(null);
+
+  // Focus the active option when the selectedOptionId changes
+  useEffect(() => {
+    activeOptionRef.current?.focus();
+  }, [selectedOptionId]);
+
   useEffect(() => {
     if (!currentChapter) return; // Exit early if no chapter.
 
@@ -68,11 +76,37 @@ const Quiz: React.FC<QuizProps> = ({
 
     const randomQuestionOrder = generateQuestionOrder();
     setQuestionOrder(randomQuestionOrder);
-  }, []); // Include chapter in dependencies
+  }, []);
 
   const [questionOrder, setQuestionOrder] = useState<number[]>([]);
 
-  const currentQuestion = currentChapter!.questions[questionOrder[questionIdx]]; // Update how the question is accessed
+  const currentQuestion = currentChapter?.questions[questionIdx];
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLButtonElement>) => {
+    if (!currentQuestion) return;
+
+    const lastOptionIndex = currentQuestion.options.length - 1;
+
+    switch (event.key) {
+      case 'ArrowDown':
+        event.preventDefault();
+        setSelectedOptionId((prevId) => Math.min(prevId + 1, lastOptionIndex));
+        break;
+      case 'ArrowUp':
+        event.preventDefault();
+        setSelectedOptionId((prevId) => Math.max(prevId - 1, 0));
+        break;
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+        setSelectedOptionId(parseInt(event.key) - 1);
+        break;
+    }
+  };
 
   return (
     <div className='max-w-7xl p-6 bg-white border border-stone-200 rounded-lg mb-6'>
@@ -81,49 +115,19 @@ const Quiz: React.FC<QuizProps> = ({
           <div className='md:flex flex-col md:justify-between gap-5'>
             <Prompt value={currentQuestion.prompt} />
 
-            {currentQuestion.options.map((option: any) => {
+            {currentQuestion.options.map((option: any, index) => {
               return (
                 <Option
                   isActive={option.id === selectedOptionId}
                   isCorrect={option.id === currentQuestion.correctId}
                   showSolution={showSolution}
+                  ref={index === selectedOptionId ? activeOptionRef : undefined}
                   key={option.id}
                   label={option.label}
                   onClick={() => {
                     setSelectedOptionId(option.id);
                   }}
-                  onKeyDown={(event: any) => {
-                    event.preventDefault(); // Preventing the page from scrolling when the arrow keys are used.
-                    switch (event.key) {
-                      case 'ArrowDown':
-                        setSelectedOptionId(selectedOptionId! + 1);
-                        break;
-                      case 'ArrowUp':
-                        setSelectedOptionId(selectedOptionId! - 1);
-                        break;
-                      case '1':
-                        setSelectedOptionId(0);
-                        break;
-                      case '2':
-                        setSelectedOptionId(1);
-                        break;
-                      case '3':
-                        setSelectedOptionId(2);
-                        break;
-                      case '4':
-                        setSelectedOptionId(3);
-                        break;
-                      case '5':
-                        setSelectedOptionId(4);
-                        break;
-                      case '6':
-                        setSelectedOptionId(5);
-                        break;
-                      case '7':
-                        setSelectedOptionId(6);
-                        break;
-                    }
-                  }}
+                  onKeyDown={handleKeyDown}
                 />
               );
             })}
@@ -176,36 +180,3 @@ const Quiz: React.FC<QuizProps> = ({
 };
 
 export default Quiz;
-
-/* import Exercise from '@/components/exercise';
-import { useState } from 'react';
-
-const pickRandExerciseNum = (min: number, max: number) =>
-  Math.floor(Math.random() * (max - min) + min);
-
-interface QuizProps {
-  numOfExercises: number;
-}
-
-const Quiz: React.FC<QuizProps> = ({ numOfExercises = 10 }) => {
-  pickRandExerciseNum(1, 60);
-  return (
-    <>
-      <div className='flex w-full h-screen overflow-scroll'>
-        <div className='p-4 w-full'>
-          <h1 className='mb-6 text-3xl font-bold text-stone-900'>Quiz</h1>
-          {[...Array(numOfExercises)].map((_, index) => (
-            <Exercise
-              chapter={3}
-              key={index}
-              initialQuestionIdx={pickRandExerciseNum(1, 60)}
-              showExerciseId={false}
-            />
-          ))}
-        </div>
-      </div>
-    </>
-  );
-};
-
-export default Quiz; */
