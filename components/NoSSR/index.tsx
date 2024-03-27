9d0a533e2afa0ef8750a79595f1a0bb53ec25659
Linus Bolls <linusbolls@Linuss-MacBook-Air.local>
2024-03-27 18:35:47 +0100

    fix: nextJs hydration mismatch for quiz

---
+++ components/NoSSR/index.tsx
@@ -0,0 +1,11 @@
+import dynamic from "next/dynamic";
+import React from "react";
+
+const NoSSR = (props: any) => <React.Fragment>{props.children}</React.Fragment>;
+
+/**
+ * based on https://stackoverflow.com/a/57173209
+ */
+export default dynamic(() => Promise.resolve(NoSSR), {
+  ssr: false,
+});
