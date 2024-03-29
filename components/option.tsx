075bac6ef8a7ab1b288a2128f9a5ac8d357ed3e1
Malik <malik@hey.com>
2024-03-29 21:07:17 +0100

    Improved styling within the quiz mode to make it more accessible.

--- components/option.tsx
+++ components/option.tsx
@@ -18,27 +18,32 @@ const Option = React.forwardRef<HTMLButtonElement, OptionProps>(
     { index, label, isActive, isCorrect, showSolution, onClick, onKeyDown },
     ref
   ) => {
-    const className = classNames(
-      'w-full ps-4 text-primary flex items-center border rounded-lg focus:outline-primary',
-      !isActive && 'border-stone-200',
-      showSolution && (isCorrect ? 'bg-green-100' : 'border-rose-200'),
-      !showSolution && isActive && 'border-primary border-2'
+    const optionClasses = classNames(
+      'w-full ps-4 text-gray-900 flex items-center border rounded-lg focus:outline-primary transition-colors duration-300',
+      {
+        'border-gray-200': !isActive && !showSolution,
+        'bg-green-200': showSolution && isCorrect,
+        'border-rose-200 text-red-500 cursor-not-allowed':
+          showSolution && !isCorrect,
+        'border-primary border-2': !showSolution && isActive,
+        'hover:border-primary focus:border-primary': !showSolution,
+      }
     );
 
     return (
       <button
         ref={ref}
         onClick={onClick}
-        className={className}
+        className={optionClasses}
         onKeyDown={onKeyDown}
       >
         <div className='flex items-center align-middle gap-3'>
           <div className='rounded-full border-2 flex wrap w-8 h-8 items-center align-middle self-center'>
-            <div className='items-center self-center font-medium w-full text-stone-900'>
+            <div className='items-center self-center font-medium w-full text-gray-900'>
               {index}
             </div>
           </div>
-          <div className='py-4 ms-2 font-medium text-stone-900'>{label}</div>
+          <div className='py-4 ms-2 font-medium'>{label}</div>
         </div>
       </button>
     );
