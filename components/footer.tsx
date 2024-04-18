3dcce1e4ec852f2a1b1f33294b99bce173f59ed1
Mark Witt <markwitt6@yahoo.de>
2024-04-18 14:07:36 +0200

    Fix svg rules

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export function Footer() {
   return (
-    <footer className='p-4 mt-4 bg-white sm:p-6'>
+    <footer className='mt-4 bg-white p-4 sm:p-6'>
       <div className='mx-auto max-w-screen-xl'>
         <div className='md:flex md:justify-between'>
           <div className='mb-6 md:mb-0'>
@@ -12,14 +12,14 @@ export function Footer() {
               className='mr-3 h-8'
               alt='FlowBite Logo'
             /> */}
-              <span className='self-center text-2xl font-semibold whitespace-nowrap'>
+              <span className='self-center whitespace-nowrap text-2xl font-semibold'>
                 LogiCola
               </span>
             </Link>
           </div>
-          <div className='grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3'>
+          <div className='grid grid-cols-2 gap-8 sm:grid-cols-3 sm:gap-6'>
             <div>
-              <h2 className='mb-6 text-sm font-semibold text-gray-900 uppercase'>
+              <h2 className='mb-6 text-sm font-semibold uppercase text-gray-900'>
                 Resources
               </h2>
               <ul className='text-gray-600'>
@@ -42,7 +42,7 @@ export function Footer() {
               </ul>
             </div>
             <div>
-              <h2 className='mb-6 text-sm font-semibold text-gray-900 uppercase'>
+              <h2 className='mb-6 text-sm font-semibold uppercase text-gray-900'>
                 Follow us
               </h2>
               <ul className='text-gray-600'>
@@ -65,7 +65,7 @@ export function Footer() {
               </ul>
             </div>
             <div>
-              <h2 className='mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white'>
+              <h2 className='mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white'>
                 Legal
               </h2>
               <ul className='text-gray-600'>
@@ -92,13 +92,13 @@ export function Footer() {
             </Link>
             . Some Rights Reserved.
           </span>
-          <div className='flex mt-4 space-x-6 sm:justify-center sm:mt-0'>
+          <div className='mt-4 flex space-x-6 sm:mt-0 sm:justify-center'>
             <Link
               href='https://twitter.com/LogicolaWeb'
               className='text-gray-500 hover:text-gray-900'
             >
               <svg
-                className='w-5 h-5'
+                className='h-5 w-5'
                 fill='currentColor'
                 viewBox='0 0 24 24'
                 aria-hidden='true'
@@ -111,15 +111,15 @@ export function Footer() {
               className='text-gray-500 hover:text-gray-900'
             >
               <svg
-                className='w-5 h-5'
+                className='h-5 w-5'
                 fill='currentColor'
                 viewBox='0 0 24 24'
                 aria-hidden='true'
               >
                 <path
-                  fill-rule='evenodd'
+                  fillRule='evenodd'
                   d='M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z'
-                  clip-rule='evenodd'
+                  clipRule='evenodd'
                 />
               </svg>
             </a>
