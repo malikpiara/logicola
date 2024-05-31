b17a9a83ca8a54a37757a6272eeb5322c97755a4
Linus Bolls <linusbolls@Linuss-MacBook-Air-3.local>
2024-05-31 20:01:19 +0200

    feat: on quiz pages, show the quiz title in the tab title

--- app/[...slugs]/page.tsx
+++ app/[...slugs]/page.tsx
@@ -1,6 +1,7 @@
 import NoSSR from '@/components/NoSSR';
 import Quiz from '@/components/quiz';
 import { chapters } from '@/content';
+import { Metadata } from 'next';
 
 const arraysAreSame = (array1: string[], array2: string[]) =>
   array1.length === array2.length &&
@@ -27,7 +28,7 @@ export default function QuizPage({ params }: QuizPageProps) {
     <div className='flex w-full h-screen'>
       {/* <ExerciseSidebar
           chapter={3}
-          path='/informal/definitions/'
+          path={params.slugs.slice(0, -1)}
           isQuestionActive={(index) => {
             return pathname === `/informal/definitions/${index + 1}`;
           }}
@@ -40,4 +41,16 @@ export default function QuizPage({ params }: QuizPageProps) {
       </main>
     </div>
   );
+}
+
+export async function generateMetadata({
+  params,
+}: QuizPageProps): Promise<Metadata> {
+  const chapter = chapters.find((i) =>
+    arraysAreSame(i.slugs.concat(['quiz']), params.slugs)
+  );
+
+  return {
+    title: (chapter?.title || 'Not found') + ' | Logicola',
+  };
 }
